400656427f6d90c7c94dbdacc125fbaa2afc5d84
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-04-21 13:36:42 +0200

    update

---
+++ EXO1.ps1
@@ -0,0 +1,12 @@
+﻿Get-ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned
+#Shell-Verbindung ExchangeOnline
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+Install-Module ExchangeOnlineManagement -Verbose -Scope AllUsers -Force
+
+Connect-ExchangeOnline
+get-command *mail*
+Get-EXOMailbox | ft displayname,emailaddresses
+Get-EXOMailbox | Get-EXOMailboxStatistics | sort totalitemsize -Descending | ft displayname,itemcount,totalitemsize 
---
+++ EXO2.ps1
@@ -0,0 +1,11 @@
+﻿$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+
+get-command *mail*
+
+Add-MailboxFolderPermission -Identity konferenzraum1:\calendar -User admin@M365x79306701.onmicrosoft.com -AccessRights owner
+get-help Add-MailboxFolderPermission -Detailed
+
+Add-MailboxPermission -Identity AllanD@M365x79306701.OnMicrosoft.com -User admin@M365x79306701.onmicrosoft.com -AccessRights fullaccess -AutoMapping:$false
+Get-MailboxFolderPermission -Identity konferenzraum1:\calendar
+Remove-MailboxFolderPermission -Identity konferenzraum1:\calendar -User admin@M365x79306701.onmicrosoft.com
