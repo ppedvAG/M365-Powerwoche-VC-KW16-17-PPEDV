4c92c81790e054f180f2d12d63797ce6e9016173
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-04-26 08:53:55 +0200

    sp

---
+++ SP-HowTos/SHP3/Shell/AzureADGruppenerstellungDeaktivieren.ps1
@@ -0,0 +1,40 @@
+﻿Get-InstalledModule -Name "AzureAD*"
+Install-Module AzureADPreview
+
+Connect-AzureAD 
+
+####
+
+$GroupName = "IchDarfGruppen"
+$AllowGroupCreation = "False"
+
+Connect-AzureAD
+
+try
+    {
+        $template = Get-AzureADDirectorySettingTemplate | ? {$_.displayname -eq "group.unified"}
+        $settingsCopy = $template.CreateDirectorySetting()
+        New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+        $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+    }
+catch
+    {
+        $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id       
+    }
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+If ($GroupName -eq "")
+    {
+        $SettingsCopy["GroupCreationAllowedGroupId"] = ""
+    }
+Else
+    {
+        $SettingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
+    }
+
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
+
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
---
+++ SP-HowTos/SHP3/Shell/o365-SharepointShell-Connection.ps1
@@ -0,0 +1,15 @@
+﻿Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable | Select Name,Version
+
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+
+##LoginName und o365-orgName eintragen!
+
+$adminUPN="LoginName"
+$orgName="OrgName"
+$userCredential = Get-Credential -UserName $adminUPN -Message "Type the password."
+Connect-SPOService -Url https://$orgName-admin.sharepoint.com -Credential $userCredential
+
+###Nur bei MFA
+
+$orgName="<name of your Office 365 organization, example: contosotoycompany>"
+Connect-SPOService -Url https://$orgName-admin.sharepoint.com
